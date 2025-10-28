d7cd5c6e14203c19a315ad35343d66090813b94a
plor <plor@nihil.wtf>
2025-10-28 17:43:54 -0500

    adds vimb and mullvad to install

--- fedora/bin/dnf_install.sh
+++ fedora/bin/dnf_install.sh
@@ -2,6 +2,7 @@
 # general tools
 sudo dnf install -y neovim stow zsh zsh-syntax-highlighting zsh-autosuggestions
 sudo dnf install -y alacritty mutt pass dnf-plugins-core pipewire git-delta
+sudo dnf install -y vimb
 
 # hyprland stuff
 sudo dnf -y copr enable solopasha/hyprland
@@ -10,3 +11,7 @@ sudo dnf install -y hyprsunset hyprshot hyprpanel waybar wob mako
 
 # hyprlock dependencies
 sudo dnf install -y cmake gcc-c++ mesa-libEGL-devel hyprwayland-scanner-devel
+
+# mullvad
+sudo dnf config-manager -y addrepo --from-repofile=https://repository.mullvad.net/rpm/beta/mullvad.repo
+sudo dnf install -y mullvad-vpn
