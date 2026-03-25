0959954d88401e40eb060c7d9608ef99a7fa3cde
plor <plor@nihil.wtf>
2026-03-25 15:32:46 -0500

    adds a couple more util programs

--- fedora/bin/dnf_install.sh
+++ fedora/bin/dnf_install.sh
@@ -2,12 +2,13 @@
 # general tools
 sudo dnf install -y neovim stow zsh zsh-syntax-highlighting zsh-autosuggestions
 sudo dnf install -y alacritty mutt pass dnf-plugins-core pipewire git-delta
-sudo dnf install -y vimb just
+sudo dnf install -y vimb just irssi lsb_release solaar zbar
 
 # hyprland stuff
 sudo dnf -y copr enable solopasha/hyprland
 sudo dnf install -y hyprland hyprpaper hypridle hyprlock hyprpanel
 sudo dnf install -y hyprsunset hyprshot hyprpanel waybar wob mako
+sudo dnf install -y wev
 
 # hyprlock dependencies
 sudo dnf install -y cmake gcc-c++ mesa-libEGL-devel hyprwayland-scanner-devel
@@ -15,3 +16,7 @@ sudo dnf install -y cmake gcc-c++ mesa-libEGL-devel hyprwayland-scanner-devel
 # mullvad
 sudo dnf config-manager -y addrepo --from-repofile=https://repository.mullvad.net/rpm/beta/mullvad.repo
 sudo dnf install -y mullvad-vpn
+
+# docker
+sudo dnf config-manager -y addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
