a85ccab55ddf48a91d6f4df6d31e4f7729e7b98d
plor <plor@nihil.wtf>
2026-09-03 13:43:11 -0500

    Non-default packages to install for omarchy

---
+++ omarchy/bin/pacman_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# general tools
+sudo pacman -S --noconfirm stow zsh zsh-syntax-highlighting zsh-autosuggestions
+sudo pacman -S --noconfirm zsh-autocomplete zsh-history-substring-search
+
+sudo pacman -S --noconfirm alacritty pass pipewire git-delta vimb just irssi
+sudo pacman -S --noconfirm lsb-release solaar zbar glow mullvad-vpn mullvad-vpn-daemon
